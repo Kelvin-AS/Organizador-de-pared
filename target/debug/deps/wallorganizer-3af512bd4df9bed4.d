7ff3e7af86f30c1dce0cba9e96aceacd352355a7
Kelvin Sanchez <Kelvinriossan@gmail.com>
2025-04-11 01:17:05 -0400

    Add files via upload

---
+++ target/debug/deps/wallorganizer-3af512bd4df9bed4.d
@@ -0,0 +1,5 @@
+C:\Users\Owner\Documents\wallorganizer-main\target\debug\deps\libwallorganizer-3af512bd4df9bed4.rmeta: src\main.rs
+
+C:\Users\Owner\Documents\wallorganizer-main\target\debug\deps\wallorganizer-3af512bd4df9bed4.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/wallorganizer-ac2745ba89a15c43.d
@@ -0,0 +1,5 @@
+C:\Users\Owner\Documents\wallorganizer-main\target\debug\deps\libwallorganizer-ac2745ba89a15c43.rmeta: src\main.rs
+
+C:\Users\Owner\Documents\wallorganizer-main\target\debug\deps\wallorganizer-ac2745ba89a15c43.d: src\main.rs
+
+src\main.rs:
